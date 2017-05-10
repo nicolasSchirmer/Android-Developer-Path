1d9e1b9748e346d60180743ecb0671c0ea4f62ae
Nícolas Meira da Silva Schirmer <nschirmer.dev@gmail.com>
2017-05-10 09:41:13 -0300

    aula 2 source code

---
+++ Appaula02/app/src/main/java/schirmer/nicolas/appaula02/User.java
@@ -0,0 +1,28 @@
+package schirmer.nicolas.appaula02;
+
+public class User {
+
+    private String name;
+    private String description;
+
+    public User(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
