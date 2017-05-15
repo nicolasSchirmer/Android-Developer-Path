a5db248dc8e50f423a62fb8a00468e527edcd2cf
Nícolas Meira da Silva Schirmer <nschirmer.dev@gmail.com>
2017-05-15 10:32:15 -0300

    source aula 6

---
+++ Aula6/app/src/main/java/schirmer/nicolas/aula6/DownloadListener.java
@@ -0,0 +1,8 @@
+package schirmer.nicolas.aula6;
+
+import android.graphics.Bitmap;
+
+public interface DownloadListener {
+
+    void getImg(Bitmap bitmap);
+}
---
+++ Aula6/app/src/main/java/schirmer/nicolas/aula6/MainActivity.java
@@ -0,0 +1,30 @@
+package schirmer.nicolas.aula6;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        DownloadImage downloadImage = new DownloadImage(this, progressBar,
+                new DownloadListener() {
+            @Override
+            public void getImg(Bitmap bitmap) {
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+    }
+}
