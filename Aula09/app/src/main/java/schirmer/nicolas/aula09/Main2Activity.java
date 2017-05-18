e848cd3e50a83355c2a2d5ea602b64e25cb7ce0a
Nícolas Meira da Silva Schirmer <nschirmer.dev@gmail.com>
2017-05-18 10:11:53 -0300

    maps + gps

---
+++ Aula09/app/src/main/java/schirmer/nicolas/aula09/Main2Activity.java
@@ -0,0 +1,77 @@
+package schirmer.nicolas.aula09;
+
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Main2Activity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mMap;
+    int x = 0, y = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        getLocation();
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.gmap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng location = new LatLng(x, y);
+        mMap.addMarker(
+                new MarkerOptions().position(location)
+                        .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+
+    private void getLocation(){
+        // dialog de permissão
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions( this,
+                    new String[] {
+                            android.Manifest.permission.ACCESS_FINE_LOCATION },
+                     101);
+        }
+
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return  ;
+        }
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        Location location = locationManager.getLastKnownLocation(
+                locationManager.getBestProvider(criteria, false)
+        );
+
+        x = (int) (location.getLatitude());
+        y = (int) (location.getLongitude());
+    }
+}
