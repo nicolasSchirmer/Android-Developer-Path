3a0e01d6098563f61493766dcfe9537c26c660e8
Nícolas Meira da Silva Schirmer <gaaratuning@gmail.com>
2017-09-14 17:06:50 -0300

    aula 03 - instagram - gridview

--- Aula03/app/src/main/java/nicolas/schirmer/aula03/MainActivity.java
+++ Aula03/app/src/main/java/nicolas/schirmer/aula03/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
 
         List<Pessoa> pessoas = new ArrayList<>();
         pessoas.add(new Pessoa("bla", R.drawable.ted));
@@ -26,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
         pessoas.add(new Pessoa("bli", R.drawable.ted));
         pessoas.add(new Pessoa("blo", R.drawable.ic_art_track_black_24dp));
 
+        ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(new CustomListAdapter(pessoas ,this));
 
+        /*
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -36,5 +38,14 @@ public class MainActivity extends AppCompatActivity {
                 //startActivity();
             }
         });
+        */
+
+        // com grid
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new CustomListAdapter(pessoas ,this));
+
+
+
     }
 }
